df4f92fb8f7fd8c5c84da398ade6f4a4df459cf7
ikarskarn <tuckermarc11@gmail.com>
2020-07-20 10:48:56 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import SideBar from "./SideBar";
+import TopBar from "./TopBar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
+}
